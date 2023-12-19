96f39d28a1d42a9aa78eb5c9b9a587de6b7f112c
Waddah Adel Elsir <chirchillw@gmail.com>
2023-12-19 15:54:20 +0100

    Update plugin.php to make the version 7.999

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = "powbi_rep_prov";
 $version = "1.0.1";
 $ilias_min_version = "5.3.0";
-$ilias_max_version = "6.999";
+$ilias_max_version = "7.999";
 $responsible = "Ralph Dittrich";
 $responsible_mail = "dittrich@qualitus.de";
 $git_path = 'git@git.qualitus.de:ilias-plugins/PowerBiReportingProvider.git';
